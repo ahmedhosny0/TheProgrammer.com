796a14a59cf5ce96cf1dbea108f0a599984f901a
ahmedhosny0 <63015510+ahmedhosny0@users.noreply.github.com>
2020-09-27 00:16:48 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,50 +1,203 @@
-
-<html>  
-<head>  
-    <meta charset="utf-8">
-    <title>تسجيل الدخول للموقع</title>   
-    <link rel = "stylesheet" type = "text/css" href = "https://raw.githack.com/ahmedhosny0/TheProgrammer.com/master/css/style.css">   
-</head>  
-<body>  
-    <div id= "frm">  
-        <h1>Login</h1>  
-        <form name="f1" action = "https://raw.githack.com/ahmedhosny0/TheProgrammer.com/master/authentication.php" onsubmit = "return validation()" method = "POST">  
-            <ul id="userandpass">
-                  <li id="Input"style="border-bottom:none;">
-                   <label>UserName: </label><input type = "text"  name  = "user" id="user" > 
-                </li>
+<!DOCTYPE html>
+    <html lang="ar">
+        <head>
+           <title>الرئيسيه - الباشمبرمج</title>
+            <meta charset="utf-8">
+            <meta name="description" content="Hi, I'm ahmed hosny ahmed khairy 'Web developer' and My channel the programmer can help you to protect your privacy. ">
+            <meta name="viewport" content="width=device-width,initial-scale=1">
+            <link rel="stylesheet" href="https://raw.githack.com/ahmedhosny0/TheProgrammer.com/master/css/bootstrap.min.css"><!-- for using container-->
+            <link rel="stylesheet" href="https://raw.githack.com/ahmedhosny0/TheProgrammer.com/master/css/st.css">
+          
+            <link rel="stylesheet" href="https://raw.githack.com/ahmedhosny0/TheProgrammer.com/master/font/css/all.min.css"> <!-- for using social media icons and other icons-->
+   </head>
+            <!-- page background-->
+      
+     
+            <body>
+                <div class="container">
+                    <!-- first div for the heading the main box-->
+                  <div class="heading">
+                    <!-- second div for the top titles-->
+                    <div class="s1">
+                        <div class="s">
+                            <ul>
+                                
+                            <li>
+                                <a href="https://www.facebook.com/%D8%A7%D9%84%D8%A8%D8%A7%D8%B4%D9%85%D8%A8%D8%B1%D9%85%D8%AC-Programmer-103597594692020/" target="_blank">
+                                    <i class="fab fa-facebook-f"></i>
+                                </a>
+                            </li>
+                            <li>
+                                 <a href="https://www.instagram.com/ahmed_hosny110/?hl=en" target="_blank">
+                                <i class="fab fa-instagram"></i>
+                                 </a>
+                            </li>
+                            <li>
+                                 <a href="https://twitter.com/Ahmed_Hosny00" target="_blank" >
+                                <i class="fab fa-twitter"></i>
+                                 </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                <i class="fab fa-snapchat"></i>
+                                     </a>
+                                    </li>
+                               
+                            <li>
+                                <a href="https://www.youtube.com/channel/UCCgXRzbbMCkC3-YKhJbkhIQ" target="_blank" >
+                                <i class="fab fa-youtube"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/in/ahmed-hosny-72a77b151/" target="_blank">
+                                <i class="fab fa-linkedin"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                 <i class="fas fa-wifi  "style="color:white" ></i>
+                            </a>
+                            </li>
+                            </ul>
+                        </div>
+                        <!-- make these titles in unordered list and inblock in css-->
+                    <ul class="u-1">
+                        <li class="l"><a href="https://raw.githack.com/ahmedhosny0/TheProgrammer.com/master/index.html"><button>المدونه</button></a></li>
+						<li class="l"><a href="https://raw.githack.com/ahmedhosny0/TheProgrammer.com/master/index.html"><button>الرئيسيه</button></a> </li>
+						<li class="l">
+                            <a href="http://localhost/Tech_Website/comment/">
+                      <button>اترك تعليق</button>
+                                
+                                </a>
+                                </li>
+                        
+                    </ul>
                     
-               <li id="Input"style="border-bottom:solid 2px red;">
-                 
-                 <label> Password:</label> <input type = "password"  name  = "pass" id="pass"> 
-                  </li>
-        
-                <input type =  "submit" id = "btn" value = "Login" />  
-            
-            </ul>
-        </form>  
-    </div>  
-    <script>  
-            function validation()  
-            {  
-                var id=document.f1.user.value;  
-                var ps=document.f1.pass.value;  
-                if(id.length=="" && ps.length=="") {  
-                    alert("User Name and Password fields are empty");  
-                    return false;  
-                }  
-                else  
-                {  
-                    if(id.length=="") {  
-                        alert("User Name is empty");  
-                        return false;  
-                    }   
-                    if (ps.length=="") {  
-                    alert("Password field is empty");  
-                    return false;  
-                    }  
-                }                             
-            }  
-        </script>  
-</body>     
-</html>  
+                  </div > <!-- end of the div num2-->
+                  <!--div num3 for the The main tile programmer-->
+                <div>
+                     <h1 class="name">الباشمبرمج </h1> <!-- make it heading-->
+                </div> <!-- end of div num3-->
+                <!-- div num4 for the bottom titles-->
+                  <div class="s2">
+                    <!-- make them in unordered list and inline block-->
+                    <ul ul class="u-1"ul>
+                        <li class="l"><a><button>الفوتوشوب</button></a></li>
+                        <li class="l"><a href="http://www.google.com" target="_blank"><button>البرمجه</button></a></li>
+                        <li class="l"><a href="#"><button> الامن المعلوماتي</button></a></li>
+                        <li class="l"><a href="#"><button> الشبكات</button></a></li>
+                        <li class="l"><a href="https://raw.githack.com/ahmedhosny0/TheProgrammer.com/master/index.html"><i class="fas fa-home" style="font-size:25px; color:red;"></i></a></li>                        
+                    </ul>
+                  </div><!-- end of the div num4-->
+                </div><!-- end of the div num1-->                
+                <!-- first div for the heading the main box-->
+            <div class="content"> <p class="ch">أخر التدوينات</p> <!--content heading-->
+                <div class="rightcol"> <!-- colum div-->
+                        <!-- video num1 -->
+                           <div class="vn1"><!-- title of video number 1-->
+                           <a href="https://raw.githack.com/ahmedhosny0/TheProgrammer.com/master/articles/photo-security.html">
+                               <h1 class="tvn1">تأمين الصور علي الموبايل بطريقه سهله</h1>
+                               <img class="ivn1" src="https://ahmed794387207.files.wordpress.com/2020/09/photo-security.jpg" alt="تأمين الصور" >
+                               </a>
+                                 <div class="clear_fix">
+                               <div class="vtext">
+                                    <p>
+                                   من المهم جدا تأمين الصور الموجوده علي الموبايل واللاب توب والاجهزه
+                                   الأخرىشعبه حسب تفضيلك طريقه معينه , ومن
+                                   افضل الأماكن هو Drive لان عن طريق الدرايف تستطيع ان توصل لصورك
+                                   عن طريق أي جهاز طالما متصل بالانترنت, وتسطيع تنزيلها علي جهازك
+                                   كن اخري..... <a href="https://raw.githack.com/ahmedhosny0/TheProgrammer.com/master/articles/photo-security.html"><button>أكمل القراءه</button></a>
+                               </p>
+                                   
+                               </div>
+                               </div>
+                           </div>
+                           <!-- video num1 -->
+                           <div class="vn1"><!-- title of video number 1-->
+                           <a href="https://raw.githack.com/ahmedhosny0/TheProgrammer.com/master/articles/router.html">
+                               <h1 class="tvn1">تأمين الراوتر من الاختراق وعمل شبكتين واي فاي</h1>
+                               <img class="ivn1" src="https://ahmed794387207.files.wordpress.com/2020/09/router-video.jpg" alt="تأمين الصور" >
+                               </a>
+                                <div class="clear_fix">
+                               <div class="vtext">
+                                   <p>
+                                  تأمين الراونر خطوه اساسيه يجب القيام بها من أجل حمايه خصوصيتك وبياناتك الشخصيه,
+                                       ومن أجل ذلك ستتعرف في هذا المقال والفيديو كيفيه عمل باسوورد قويه وتغيير اسم المستخدم
+                                       وضبط اعدادات الحمايه والتشفير...واشياء أخري اساسيه بالاضافه الي عمل شبكتين واي فاي
+                                       .....<a href="https://raw.githack.com/ahmedhosny0/TheProgrammer.com/master/articles/router.html"><button>أكمل القراءه</button></a>
+                               </p>
+                               </div>
+                               </div>
+                           </div>
+                       <!-- video num1 -->
+                           <div class="vn1"><!-- title of video number 1-->
+                            <a href="https://raw.githack.com/ahmedhosny0/TheProgrammer.com/master/articles/whats.html">
+                               <h1 class="tvn1">تأمين الواتساب بطريقه احترافيه</h1>
+                             
+                           <img class="ivn1" src="https://ahmed794387207.files.wordpress.com/2020/09/whats-video.jpg" alt="تأمين الصور" >
+                               </a>
+                                 <div class="clear_fix">
+                               <div class="vtext">
+                                   <p >
+                                  اصبح واتس اب فى الوقت الحالى واحدا من اهم واشهر التطبيقات التى لا غنى عنها لمستخدمى الانترنت فى جميع انحاء العالم ، لذا توجب علينا استعراض لبعض مميزات هذا التطبيق الرائع والمميز وأهميته وكيفية تأمين حساب الواتس اب الخاص بك . 
+                                       وعن طريقة تامين الواتس اب الخاص بك من خلاص ميزة جديدة أصدرها واتساب مع التحديث الجديد للتطبيق .....
+                                   <a href="https://raw.githack.com/ahmedhosny0/TheProgrammer.com/master/articles/whats.html"><button>أكمل القراءه</button></a>
+                               </p>
+                               </div></div>
+                           </div>
+                             <!-- video num1 -->
+                           <div class="vn1"><!-- title of video number 1-->
+                           <a href="https://raw.githack.com/ahmedhosny0/TheProgrammer.com/master/articles/facebook.html">
+                               <h1 class="tvn1">!حسابك علي فيسبوك في خطر</h1>
+                                   <img class="ivn1" src="https://ahmed794387207.files.wordpress.com/2020/09/face-video.jpg" alt="تأمين الصور" >
+                                   </a>
+                                 <div class="clear_fix">
+                               <div class="vtext">
+                                    <p>
+                                   هذا الحساب الخاص بك عليها معظم صورك
+                                   ومحاداثات قمت باجرائها , لذلك يجب عليك تأمينه جيدا حتي لا ينتحل
+                                   شخص أخر شخصيتك ويقوم بعمل أعمال غير قانوينه وذلك عن طريق
+                                   حسابك علي فيس بوك وانت قد تتهاون مع الموضوع ولكن هناك من يعرف
+                                   قيمته جيدا.....<a href="https://raw.githack.com/ahmedhosny0/TheProgrammer.com/master/articles/facebook.html"><button>أكمل القراءه</button></a>
+                               </p>
+                               </div>
+                               </div>
+                               </div>
+                           <!-- video num1 -->
+                           <div class="vn1"><!-- title of video number 1-->
+                           <a href="https://raw.githack.com/ahmedhosny0/TheProgrammer.com/master/articles/download1.html">
+                               <h1 class="tvn1"> تنزيل مقاطع (صوت-فيديو)من يوتيوب</h1>
+                               <img class="ivn1" src="https://ahmed794387207.files.wordpress.com/2020/09/video-download-video.jpg" alt="تأمين الصور" >
+                               </a>
+                               <div class="clear_fix">
+                               <div class="vtext">
+                                   <p>
+                                       بدون استثناء اصبح الجميع يستخدم موقع يوتيوب فهناك من يستمتعون بمشاهدة مقاطع الفيديو المختلفة من افلام ودروس وأيضا القرأن الكريم , وهناك من يرغب 
+                                       تحميل الصوت فقط من فيديو يوتيوب فقط . ليقوم بالاستماع الى درس تعليمي , أو الاستماع الى القرأن الكريم .او استخدام هذا المقطع في برامج
+                                       المونتاج او عمل الفديوهات لرفعها علي اليوتيوب 
+                                   ....<a href="https://raw.githack.com/ahmedhosny0/TheProgrammer.com/master/articles/download1.html"><button>أكمل القراءه</button></a>
+                               </p>
+                               </div>
+                               </div>
+                           </div>
+                          <!-- video num1 -->
+                           <div class="vn1"><!-- title of video number 1-->
+                           <a href="https://raw.githack.com/ahmedhosny0/TheProgrammer.com/master/articles/download2.html">
+                               <h1 class="tvn1"> تنزيل مقاطع (صوت-فيديو)من فيسبوك</h1>
+                               <img class="ivn1" src="https://ahmed794387207.files.wordpress.com/2020/09/sound-download-video.jpg" alt="تأمين الصور" >
+                               </a>
+                               <div class="clear_fix">
+                               <div class="vtext">
+                                   <p>
+                                    يعتبر فيسبوك منصة التواصل الاجتماعي الأكثر استخداماً عبر الإنترنت، لما تقدّمه من خدمات المنشورات ونشر الصور والفيديوهات وغيرها من المواد والوسائط التفاعلية. وتعتبر الفيديوهات المنشورة على فيسبوك من أبرز وأسهل هذه الوسائط مشاهدةً، وقد يخطر ببال أحدهم لو أنّ هناك إمكانيّة لتحميل هذه الفيديوهات سواء إلى جهاز الكمبيوتر أو إلى الهاتف الذكي.....<a href="https://raw.githack.com/ahmedhosny0/TheProgrammer.com/master/articles/download2.html"><button>أكمل القراءه</button></a>
+                               </p>
+                               </div>
+                               </div>
+                        </div>
+                </div>
+            </div>
+                </div>
+            </body>
+       
+       
+    </html>
